b119cbd51ed06a7599c61d9f2b2ac8938912684d
KaiaMc <94264300+KaiaMc@users.noreply.github.com>
2024-04-13 15:17:48 +0300

    Delete Koneko directory

--- Koneko/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript to toggle the visibility of the menu
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var nav = document.getElementById('mobile-nav');
-    var nav = document.getElementById('mobile-socials');
-    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-    this.classList.toggle('active');
-});
